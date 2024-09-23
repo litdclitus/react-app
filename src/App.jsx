947877462161aa58c81fef3a753f3bd7b369c3be
litdclitus <dangnh799@gmail.com>
2024-09-23 16:31:15 +0700

    preview-test

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyComponent from './components/learn/MyComponent'
+import { SecondComponent, RandomComponent } from './components/learn/SecondComponent'
 
 const App = () => {
   const [count, setCount] = useState(0)
+
   return (
     <>
       <div>
@@ -16,6 +19,9 @@ const App = () => {
         </a>
       </div>
       <h1>Hello world</h1>
+      <MyComponent />
+      <SecondComponent />
+      <RandomComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,13 @@
+const SecondComponent = () => {
+    return (
+        <div>Second Component</div>
+    );
+}
+
+const RandomComponent = () => {
+    return (
+        <div>Random component</div>
+    );
+}
+
+export { SecondComponent, RandomComponent };
