1f6019e5f0b2ba3dcbf54080f3a9ab0660cf89ec
litdclitus <dangnh799@gmail.com>
2024-09-24 16:34:57 +0700

    design UI TodoList

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,11 @@
+
+const TodoData = () => {
+    return (
+        <div className="todo-data">
+            <div>learning English</div>
+            <div>watching Youtube</div>
+            <div>playing Video game</div>
+        </div>
+    );
+}
+export default TodoData;
---
+++ src/components/todo/todonew.jsx
@@ -0,0 +1,10 @@
+
+const TodoNew = () => {
+    return (
+        <div className="todo-input">
+            <input type="text" id="input-box" placeholder='Enter your task' />
+            <button id="input-btn">Add</button>
+        </div>
+    );
+}
+export default TodoNew;
