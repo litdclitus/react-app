74e401ae3d1e2e97ee06759d0b2a0ca7e9b3d1c0
litdclitus <dangnh799@gmail.com>
2024-10-11 22:34:33 +0700

    fix header footer UI

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -9,29 +9,19 @@ const Footer = () => {
 
     return (
         <div>
-            <footer>
-                <div className="footer">
-                    <div className="row">
-                        <a href="https://www.facebook.com/" target='_blank'><i><FaFacebookF /></i></a>
-                        <a href="https://github.com/litdclitus" target='_blank'><i><FaGithub /></i></a>
-                        <a href="mailto:dangnh799@gmail.com" target='_blank'><i><BiLogoGmail /></i></a>
-                    </div>
+            <div class="clear"></div>
 
-                    {/* <div className="row">
-                        <ul>
-                            <li><a href="#">Contact us</a></li>
-                            <li><a href="#">Our Services</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Terms & Conditions</a></li>
-                            <li><a href="#">Career</a></li>
-                        </ul>
-                    </div> */}
-
-                    <div className="row">
-                        &copy;<span>{yearNow} LitDClitus. All rights reserved.</span>
-                    </div>
+            <div className="footer">
+                <div className="row">
+                    <a href="https://www.facebook.com/" target='_blank'><i><FaFacebookF /></i></a>
+                    <a href="https://github.com/litdclitus" target='_blank'><i><FaGithub /></i></a>
+                    <a href="mailto:dangnh799@gmail.com" target='_blank'><i><BiLogoGmail /></i></a>
                 </div>
-            </footer>
+                <div className="row">
+                    &copy;<span>{yearNow} LitDClitus. All rights reserved.</span>
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,27 +1,32 @@
 import './header.css';
-import { Outlet, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { RxDashboard } from "react-icons/rx";
 import { HiOutlineUsers } from "react-icons/hi2";
 import { GiBlackBook } from "react-icons/gi";
-import $ from 'jquery';
-
 
 const Header = () => {
-
-
 
     return (
         <ul className="nav-links">
             <li className='nav-link-item'>
-                <i className='nav-link-icon'><RxDashboard /></i>
-                <NavLink to="/"> Dashboard</NavLink>
+                <NavLink to="/" className="item-icon">
+                    <span style={{ marginRight: 5, marginTop: 2 }}> <RxDashboard /></span>
+                    <span>Dashboard</span>
+                </NavLink>
             </li>
             <li className="nav-link-item center">
-                <i><HiOutlineUsers /></i>
-                <NavLink to="/users">User</NavLink></li>
+
+                <NavLink to="/users" className="item-icon">
+                    <span style={{ marginRight: 5, marginTop: 2 }}> <HiOutlineUsers /></span>
+                    <span>User</span>
+                </NavLink>
+            </li>
             <li className="nav-link-item center">
-                <i><GiBlackBook /></i>
-                <NavLink to="/books">Books</NavLink></li>
+                <NavLink to="/books" className="item-icon">
+                    <span style={{ marginRight: 5, marginTop: 2 }}> <GiBlackBook /></span>
+                    <span>Books</span>
+                </NavLink>
+            </li>
         </ul>
     )
 }
