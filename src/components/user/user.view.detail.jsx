c82c702782b2cfb270fd798717ac78207e197a5d
litdclitus <dangnh799@gmail.com>
2024-10-13 23:26:25 +0700

    add avatar-upload interface

--- src/components/user/user.view.detail.jsx
+++ src/components/user/user.view.detail.jsx
@@ -1,4 +1,5 @@
-import { Drawer } from 'antd';
+import { Drawer, Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 
 const ViewUserDetail = (props) => {
 
@@ -6,10 +7,13 @@ const ViewUserDetail = (props) => {
 
     return (
         <>
-            <Drawer title="User information" onClose={() => {
-                setDataDetail(null)
-                setDataModalOpen(false)
-            }}
+            <Drawer
+                width={"40vw"}
+                title="User information"
+                onClose={() => {
+                    setDataDetail(null)
+                    setDataModalOpen(false)
+                }}
                 open={dataModalOpen}>
                 {dataDetail ? <>
                     <p>Id: {dataDetail._id}</p>
@@ -19,6 +23,36 @@ const ViewUserDetail = (props) => {
                     <p>Email: {dataDetail.email}</p>
                     <br />
                     <p>Phone number: {dataDetail.phone}</p>
+                    <br />
+                    <div style={{
+                        display: "block",
+                        height: "200px", width: "200px",
+                        border: "1px solid #ccc", borderRadius: 30,
+                        marginTop: 20, marginBottom: 20,
+                        marginLeft: "auto", marginRight: "auto"
+                    }}>
+                        <img
+                            style={{
+                                height: "100%", width: "100%", objectFit: "contain%", borderRadius: 30
+                            }}
+                            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`
+                            } alt="avatar" />
+                        {/* <Button type="primary" icon={<UploadOutlined />} block
+                        >Upload avatar</Button> */}
+                    </div>
+                    <div>
+                        <input type="file" id='btnUpload' hidden />
+                        <label style={{
+                            backgroundColor: "#24a0ed", color: "white",
+                            padding: 10, fontFamily: "sans-serif",
+                            borderRadius: 20, cursor: "pointer",
+                            marginLeft: "auto", marginRight: "auto",
+                            display: "block", textAlign: "center"
+                        }}
+                            htmlFor="btnUpload">
+                            <UploadOutlined style={{ marginRight: 7 }} />
+                            Upload avatar</label>
+                    </div>
                 </>
                     :
                     <>
