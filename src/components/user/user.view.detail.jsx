355546e3896bd319d142a6541c8b955421514d8f
litdclitus <dangnh799@gmail.com>
2024-10-14 15:24:34 +0700

    add preview upload single avatar

--- src/components/user/user.view.detail.jsx
+++ src/components/user/user.view.detail.jsx
@@ -1,9 +1,29 @@
 import { Drawer, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 const ViewUserDetail = (props) => {
 
     const { dataDetail, setDataDetail, dataModalOpen, setDataModalOpen } = props;
+
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [preview, setPreview] = useState(null)
+
+    const handleOnChangeUpload = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null);
+            setPreview(null);
+            return;
+        }
+        // I've kept this example simple by using the first image instead of multiple
+        const file = event.target.files[0];
+        if (file) {
+            setSelectedFile(file)
+            setPreview(URL.createObjectURL(file))
+        }
+    }
+
+    console.log(">>check preview: ", preview)
 
     return (
         <>
@@ -37,11 +57,11 @@ const ViewUserDetail = (props) => {
                             }}
                             src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`
                             } alt="avatar" />
-                        {/* <Button type="primary" icon={<UploadOutlined />} block
-                        >Upload avatar</Button> */}
                     </div>
                     <div>
-                        <input type="file" id='btnUpload' hidden />
+                        <input type="file" id='btnUpload' hidden
+                            onChange={(event) => { handleOnChangeUpload(event) }}
+                        />
                         <label style={{
                             backgroundColor: "#24a0ed", color: "white",
                             padding: 10, fontFamily: "sans-serif",
@@ -58,6 +78,22 @@ const ViewUserDetail = (props) => {
                     <>
                         <p>No information available</p>
                     </>
+                }
+                {preview &&
+                    <div style={{
+                        display: "block",
+                        height: "80px", width: "80px",
+                        border: "1px solid #ccc",
+                        marginTop: 20, marginBottom: 10,
+                        // marginLeft: "auto", marginRight: "auto"
+                    }}>
+                        <img
+                            style={{
+                                height: "100%", width: "100%", objectFit: "contain%",
+                            }}
+                            src={`${preview}`
+                            } alt="avatar" />
+                    </div>
                 }
 
             </Drawer >
